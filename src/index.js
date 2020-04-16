5250e058ac8d8407fe2ff00a00e715287463abe2
ouanling <rkt1337@gmail.com>
2020-04-17 03:53:57 +0700

    case bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import {counterItems} from "./todos.js";
+import {createItem} from "./todos.js";
 
-const item1 = counterItems();
-item1();
-item1();
+const item1 = createItem("chingchong", "mettre chingchong au poubelle", "demain", "super");
+console.log(item1.toObj());
+item1.mod("title", "jeter le ching chong");
+console.log(item1.toObj());
 
 
--- src/todos.js
+++ src/todos.js
@@ -1,13 +1,34 @@
 
 
-const counterItems = () => {
-    let count = 0;
-    return () => {
-        console.log(count);
-        count++;
-        
-    };
+const createItem = (a, b, c, d) => {
+    const title = a;
+    const desc = b;
+    const duedate = c;
+    const priority = d;
+    const mod = (tomod, valeur) => {
+        console.log(tomod, valeur);
+        switch (tomod) {
+            
+            case "title":
+                title = valeur;
+                break;
+            case "desc":
+                desc = valeur;
+                break;
+            case "duedate":
+                duedate = valeur;
+                break;
+            case "priority":
+                priority = valeur;
+                break;
+        }
+     };
+     const toObj = () => {
+         return {title,desc,duedate,priority}
+     };
+     return {title, desc,duedate,priority,mod,toObj}
+    
 };
-const counterProjects = () => {};
 
-export {counterItems}
+
+export {createItem}
